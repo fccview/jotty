b003526d23f5c9183b25a02a3c4b838b2d4c21f0
fccview <f.campioneuk@gmail.com>
2026-02-06 13:54:02 +0000

    fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig([{
     extends: [...nextCoreWebVitals],
@@ -12,5 +7,13 @@ export default defineConfig([{
     rules: {
         "@next/next/no-img-element": "off",
         "react-hooks/exhaustive-deps": "off",
+        "react-hooks/set-state-in-effect": "off",
+        "react-hooks/purity": "off",
+        "react-hooks/refs": "off",
+        "react-hooks/preserve-manual-memoization": "off",
+        "react-hooks/immutability": "off",
+        "react-hooks/set-state-in-render": "off",
+        "react-hooks/use-memo": "off",
+        "@next/next/no-html-link-for-pages": "off",
     },
 }]);
